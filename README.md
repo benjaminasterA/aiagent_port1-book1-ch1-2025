cd14c560596e47eec5eed7e6c8f7a941a7952577
benjaminasterA <smlee0802@gmail.com>
2025-12-29 12:25:05 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# aiagent_port1-book1-ch1-2025" 
+"# 이수민 아나콘다 배우다"
+"# aiagent_port1-book1-ch1-2025" 
---
+++ ch01ex02.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+scores = [70, 75, 80, 85, 50, 35]
+
+plt.plot(scores)
+plt.title("Score Trend")
+plt.xlabel("Index")
+plt.ylabel("Score")
+plt.show()
